56aa8480ba03c0fae57aa60842582ad1fd10db84
AverageAtBest000 <octaviogamez466@gmail.com>
2026-07-13 20:13:44 -0600

    chore: make units more consistent

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ num_seconds = 100e-9
 num_samples = 5000
 
 X = np.linspace(0,num_seconds, num_samples)
-Y = generator.get_PMT_signal(A = 100, T = X, t_0 = 10, Tao_fall_nano=2.1, Tao_rise_nano=0.9, Tao_fall_spe_nano = 6 , Tao_rise_spe_nano = 2)
+Y = generator.get_PMT_signal(num_photoelectrons = 100, T = X, t_0 = 10e-9, Tao_fall=2.1e-9, Tao_rise=0.9e-9, Tao_fall_spe = 6e-9 , Tao_rise_spe = 2e-9)
 
 plt.plot(X,Y)
 plt.show() 
