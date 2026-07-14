0012cf5b4530b31f7f581637e533b9d8cb11d9f1
AverageAtBest000 <octaviogamez466@gmail.com>
2026-07-13 19:54:27 -0600

    normalize double exponential function and add file for Amplifier class

--- Digitizer.py
+++ Digitizer.py
@@ -18,23 +18,28 @@ class Digitizer:
         discrete_times = np.linspace(t_0, t_final, (t_final - t_0)/sample_period)
         voltage_samples = np.zeros(len(discrete_times))
         
-        for i in (len(discrete_times)):
+        for i in range(len(discrete_times)):
             
             if np.any( time_array == discrete_times[i]):
-                voltage_samples[i] = voltage_array[i]
+                voltage_samples[i] = voltage_array[time_array == discrete_times[i]][0]           
             else:
                 voltage_samples[i] = cls.interpolate(time_array, voltage_array, discrete_times[i])
                 
-        clipped_array  = np.clip(voltage_array, min_volts, max_volts)
-        clipped = np.array_equal(voltage_array, clipped_array)
+        clipped_voltage_samples = np.clip(voltage_samples, min_volts, max_volts)
+        was_clipped = np.array_equal(voltage_samples, clipped_voltage_samples)
         
         voltage_span = max_volts - min_volts
         least_significant_bit = (voltage_span) / ( 2 ** num_bits)
         
-        #Floor being used for quantization - document this 
         
-        Digitized = np.floor( (clipped - min_volts) / least_significant_bit  )
-
+        #Floor being used for quantization - document this 
+        Digitized_array = np.floor( (clipped_voltage_samples - min_volts) / least_significant_bit  )
+        Reconstructed_array = min_volts + (Digitized_array + 1/2)*least_significant_bit
+        
+        
+        return Digitized_array, Reconstructed_array, was_clipped
+        
+        
     @classmethod
     def interpolate(cls, X, Y, val ) -> float :
         return np.interp(val, X, Y)
