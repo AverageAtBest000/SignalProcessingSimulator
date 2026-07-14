"fix: add normalization for signal generation and make units more consistent "

Digitizer.py
@@ -18,8 +18,7 @@ class Digitizer:
         discrete_times = np.linspace(t_0, t_final, (t_final - t_0)/sample_period)
         voltage_samples = np.zeros(len(discrete_times))
         
-        for i in range(len(discrete_times)):
-            
+        for i in range(len(discrete_times)): 
             if np.any( time_array == discrete_times[i]):
                 voltage_samples[i] = voltage_array[time_array == discrete_times[i]][0]           
             else:
@@ -30,7 +29,6 @@ class Digitizer:
         
         voltage_span = max_volts - min_volts
         least_significant_bit = (voltage_span) / ( 2 ** num_bits)
-        
         
         #Floor being used for quantization - document this 
         Digitized_array = np.floor( (clipped_voltage_samples - min_volts) / least_significant_bit  )

main.py
@@ -6,10 +6,10 @@ from signalGeneration import Generator
 
 generator = Generator()
 
-num_nanoseconds = 100
+num_seconds = 100e-9
 num_samples = 5000
 
-X = np.linspace(0,num_nanoseconds, num_samples)
+X = np.linspace(0,num_seconds, num_samples)
 Y = generator.get_PMT_signal(A = 100, T = X, t_0 = 10, Tao_fall_nano=2.1, Tao_rise_nano=0.9, Tao_fall_spe_nano = 6 , Tao_rise_spe_nano = 2)
 
 plt.plot(X,Y)

signalGeneration.py
@@ -5,12 +5,12 @@ import scipy.stats as stats
 class Generator: 
     
     @classmethod
-    def get_double_exponential(cls, A, T, t_0, Tao_fall, Tao_rise):
-        raw_wave =  A * ( np.exp(-(T-t_0)/Tao_fall) - np.exp( -(T-t_0)/Tao_rise )  )
+    def get_double_exponential(cls, num_photoelectrons, T, t_0, Tao_fall, Tao_rise):
+        raw_wave =  num_photoelectrons / (Tao_fall - Tao_rise) * ( np.exp(-(T-t_0)/Tao_fall) - np.exp( -(T-t_0)/Tao_rise )  )
         return np.where(T >= t_0, raw_wave, 0)
     
     @classmethod
-    def get_PMT_signal(cls, A, T, t_0, Tao_fall_nano, Tao_rise_nano, Tao_fall_spe_nano, Tao_rise_spe_nano):
+    def get_PMT_signal(cls, num_photoelectrons, T, t_0, Tao_fall_nano, Tao_rise_nano, Tao_fall_spe_nano, Tao_rise_spe_nano):
         
         num_samples = len(T)
         dt = T[1] - T[0]
@@ -18,8 +18,10 @@ class Generator:
         signal = np.zeros(num_samples)
         rng = np.random.default_rng()
         
-        expected = cls.get_double_exponential(A, T, t_0, Tao_fall_nano, Tao_rise_nano)
+        expected = cls.get_double_exponential(num_photoelectrons, T, t_0, Tao_fall_nano, Tao_rise_nano)
         expected = np.clip(expected, 0, None) * dt
+        
+        
         
         arrival_noise = stats.poisson.rvs(expected, size = len(expected))    
         