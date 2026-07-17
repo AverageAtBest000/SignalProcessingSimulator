a510018c9ddbe0f417939cade83a9f53bf3aa4c9
AverageAtBest000 <octaviogamez466@gmail.com>
2026-07-17 01:17:07 -0600

    feat: modify Terminator class to be compatible with cable class

--- ComponentClasses/Terminators.py
+++ ComponentClasses/Terminators.py
@@ -1,73 +1,34 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[4]:
-
-
-#code BEFORE it got added to a class
-import numpy as np
-
-def mess_up_signal(original_signal, sample_rate, delay_seconds, reflection_coefficient=0.5):
-
-    
-    shift_samples = int(delay_seconds * sample_rate)
-    
-    reflected_signal = np.zeros_like(original_signal)
-    
-    if shift_samples < len(original_signal):
-        reflected_signal[shift_samples:] = original_signal[:-shift_samples] * reflection_coefficient
-    
-    # Combine original signal and reflection
-    corrupted_signal = original_signal + reflected_signal
-    
-    return corrupted_signal, reflected_signal
-
-
-# In[5]:
-
-
-#code AFTER it got added to a class
 import numpy as np
 
 class Terminator:
     
-    def __init__(self, sample_rate, delay_seconds, reflection_coefficient=0.5):
-        self.sample_rate = sample_rate
-        self.delay_seconds = delay_seconds
-        self.reflection_coefficient = reflection_coefficient
-
-    def apply_reflection(self, original_signal):
-        shift_samples = int(self.delay_seconds * self.sample_rate)
+    def __init__(self, impedance):
         
-        reflected_signal = np.zeros_like(original_signal)
+        self.impedance = impedance
+
+
+    def get_ref_coefficient(self, characteristic_impedance):
+
+        if np.isinf(self.impedance):
+            return 1.0
         
-        if shift_samples < len(original_signal):
-            reflected_signal[shift_samples:] = original_signal[:-shift_samples] * self.reflection_coefficient
-        
-        # Combine original signal and reflection that came back
-        corrupted_signal = original_signal + reflected_signal
-        
-        return corrupted_signal, reflected_signal
-    
-    
-# :)    
-    
-#--------------------------------------------------------
-#how to use it in a pre-existing signal thing
+        return (self.impedance - characteristic_impedance) / (self.impedance + characteristic_impedance)
 
-# 1. Define your original signal data
-my_signal = np.sin(np.linspace(0, 10, 1000))
+    def apply_reflection(self, 
+                        cable, 
+                        time_array, 
+                        voltage_array, 
+                        source_impedance,
+                        signal_baseline, 
+                        max_round_trips,
+                        input_voltage_is_open_circuit=True):
 
-# 2. Initialize the class with your parameters
-corrupter = SignalCorrupter(sample_rate=44100, delay_seconds=0.05, reflection_coefficient=0.4)
-
-# 3. Process the signal
-corrupted, reflection = corrupter.apply_reflection(my_signal)
-
-
-
-# In[ ]:
-
-
-
-
+        return cable.propagation(
+            time_array=time_array,
+            voltage_array=voltage_array,
+            source_impedance=source_impedance,
+            load_impedance=self.impedance,
+            signal_baseline=signal_baseline,
+            max_round_trips=max_round_trips,
+            input_voltage_is_open_circuit=input_voltage_is_open_circuit,
+        )
