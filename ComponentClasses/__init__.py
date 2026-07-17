d06a0f8d92b1db2215267257e9aa567cfd2e3018
AverageAtBest000 <octaviogamez466@gmail.com>
2026-07-17 09:40:33 -0600

    feat: add __init__.py for easy importing in main

---
+++ ComponentClasses/__init__.py
@@ -0,0 +1,9 @@
+
+from .Amplifier import Amplifier
+from .Cable import Cable
+from .ConstantFractionDiscriminator import ConstantFractionDiscriminator
+from .Digitizer import Digitizer
+from .LeadingEdgeDiscriminator import EdgeDiscriminator
+from .SignalGenerator import Generator
+from .Splitter import Splitter
+from .Terminators import Terminator
