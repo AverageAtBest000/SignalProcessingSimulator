3ede3e8d6fc1739e692d7d2dc574917a66ab1006
AverageAtBest000 <octaviogamez466@gmail.com>
2026-07-24 23:00:31 -0600

    feat: add slew rate to amplifier

--- ComponentClasses/Amplifier.py
+++ ComponentClasses/Amplifier.py
@@ -3,24 +3,10 @@ import scipy.signal as signal
 
 class Amplifier:
     #saturating upper/lower can extend signals
-    def __init__(self, gain, gain_units, input_impedance, output_impedance, min_voltage_out, max_voltage_out, low_cutoff_freq = None, high_cutoff_freq = None):
-    # just use unitless
-        if gain_units.lower() == "db":
-            self.gain = 10 ** (gain/20)
-        elif gain_units.lower() == "unitless":
-            self.gain = gain        
-        else: 
-            raise ValueError("Invalid gain unit. Use \"db\" or \"unitless\" ")
 
-        if min_voltage_out > max_voltage_out:
-            raise ValueError("max_voltage_out must be greater than min_voltage_out")
+    def __init__(self, gain, gain_units, input_impedance, output_impedance, min_voltage_out, max_voltage_out, slew_rate_up = np.inf, slew_rate_down = -np.inf, low_cutoff_freq = None, high_cutoff_freq = None):
 
-        if low_cutoff_freq is not None and high_cutoff_freq is not None:
-            if low_cutoff_freq > high_cutoff_freq:
-                raise ValueError("high_cutoff_freq must be greater than low_cutoff_freq")
-
-        if input_impedance < 0 or output_impedance < 0:
-            raise ValueError("input_impedance and output_inpedance must be greater than zero")
+        self.validate_constructor_params(gain_units, min_voltage_out, max_voltage_out, low_cutoff_freq, high_cutoff_freq, output_impedance, input_impedance, slew_rate_up, slew_rate_down)
 
         self.low_cutoff_freq = low_cutoff_freq
         self.high_cutoff_freq = high_cutoff_freq
@@ -28,10 +14,10 @@ class Amplifier:
         self.output_impedance = output_impedance
         self.max_voltage_out = max_voltage_out
         self.min_voltage_out = min_voltage_out
+        self.slew_rate = slew_rate
 
 
     def amplify(self, time_array: np.ndarray, loaded_voltage_array: np.ndarray, signal_baseline: float, output_baseline: float = 0.0) -> tuple[np.ndarray, np.ndarray]:
-    # some of this only aplies to RF signal
         
         self.validate_params(time_array, loaded_voltage_array)
         time_delta = time_array[1] - time_array[0]
@@ -60,8 +46,21 @@ class Amplifier:
 
         open_circuit_amplified_voltage = np.clip(open_circuit_amplified_voltage, self.min_voltage_out, self.max_voltage_out)
 
+        max_voltage_delta_up = self.slew_rate * time_delta
+        max_voltage_delta_down = -max_voltage_delta_down
+
+        for i in range(1, len(open_circuit_amplified_voltage)):
+
+            current_delta = open_circuit_amplified_voltage[i] - open_circuit_amplified_voltage[i-1]
+            
+            if current_delta > max_voltage_delta_up:
+                open_circuit_amplified_voltage[i] = open_circuit_amplified_voltage[i-1] + max_voltage_delta_up * time_delta
+            elif current_delta < max_voltage_delta_down:
+                open_circuit_amplified_voltage[i] = open_circuit_amplified_voltage[i-1] + max_voltage_delta_down * time_delta
+
         return time_array, open_circuit_amplified_voltage
 
+    
     def validate_params(self, time_array, loaded_voltage_array):
         
         if len(time_array) != len(loaded_voltage_array) or len(time_array) < 2:
@@ -74,6 +73,33 @@ class Amplifier:
         if self.low_cutoff_freq is not None:
             if self.low_cutoff_freq <= 0:
                 raise ValueError("high_cutoff_freq and low_cutoff_freq must be greater than 0")
+
+    
+    def validate_constructor_params(self, gain_units, min_voltage_out, max_voltage_out, low_cutoff_freq, high_cutoff_freq, output_impedance, input_impedance, slew_rate_up, slew_rate_down):
+
+        if gain_units.lower() == "db":
+            self.gain = 10 ** (gain/20)
+        elif gain_units.lower() == "unitless":
+            self.gain = gain        
+        else: 
+            raise ValueError("Invalid gain unit. Use \"db\" or \"unitless\" ")
+        
+        if min_voltage_out > max_voltage_out:
+            raise ValueError("max_voltage_out must be greater than min_voltage_out")
+
+        if low_cutoff_freq is not None and high_cutoff_freq is not None:
+            if low_cutoff_freq > high_cutoff_freq:
+                raise ValueError("high_cutoff_freq must be greater than low_cutoff_freq")
+        
+        if input_impedance < 0 or output_impedance < 0:
+            raise ValueError("input_impedance and output_inpedance must be greater than zero")
+        
+        if slew_rate_down >= 0:
+            raise ValueError("slew_rate_down must be less than zero")
+
+        if slew_rate_up <= 0:
+            raise ValueError("slew_rate_up must be greater than zero")
+
 
     @classmethod
     def apply_high_freq_cutoff(cls, voltage_in, cutoff_frequency, time_delta):
