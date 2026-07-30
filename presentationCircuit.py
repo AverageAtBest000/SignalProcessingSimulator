736616a53bcae6cc84c4a2ca4b9920235d13d82a
AverageAtBest000 <octaviogamez466@gmail.com>
2026-07-30 07:51:00 -0600

    feat: add more realistic values to

--- presentationCircuit.py
+++ presentationCircuit.py
@@ -12,19 +12,32 @@ from ComponentClasses import (  Amplifier, Connector,
 
 
 
+velocity_factor = .66
+attenuation_db_per_m = .30
+cable_characteristic_impedance = 50
 
-num_seconds = 100e-9
+num_seconds = 150e-9
 num_samples = 10_000
 
 time_array = np.linspace(0, num_seconds, num_samples)
 
 
-amplifier = Amplifier( 500, "unitless", 50, 50, -np.inf, np.inf, slew_rate_down = -np.inf, slew_rate_up = np.inf)
+amplifier = Amplifier(  gain = 5, 
+                        gain_units = "unitless", 
+                        input_impedance = 50, 
+                        output_impedance = 50, 
+                        min_voltage_out = -2.0,
+                        max_voltage_out = 2.0, 
+                        slew_rate_down = -np.inf, 
+                        slew_rate_up = np.inf,
+                        high_cutoff_freq = 250e6 
+                        )
+
 digitizer = Digitizer(input_impedance = 60)
 
 """====================== GENERATOR INITIALIZATION ======================"""
 generator_output_impedance = 50
-generator = Generator(output_impedance = 50)
+generator = Generator(output_impedance = generator_output_impedance)
 
 expected_photoelectrons = 20
 signal_start_time = 20e-9
@@ -42,27 +55,25 @@ generated_signal  = generator.get_PMT_signal(
                                             Tau_rise_spe = Tau_rise_spe,
                                             Tau_fall_spe = Tau_fall_spe,
                                             polarity = polarity,
+                                            random_seed = 1
                                             ) 
 
 
 """====================== CABLE 1 INITIALIZATION ======================"""
 
 cable_length_meters = 1 
-velocity_factor = .7
-attenuation_db_per_m = 50
-
 cable_to_amplifier = Cable( length_m = cable_length_meters,
                             velocity_factor = velocity_factor,
                             attenuation_db_per_m = attenuation_db_per_m,
-                            characteristic_impedance = 50
+                            characteristic_impedance = cable_characteristic_impedance
                             )
 
-_, voltage_array = cable_to_amplifier.propagation( time_array = time_array, 
+_, amplifier_input_signal = cable_to_amplifier.propagation( time_array = time_array, 
                                                 open_circuit_voltage_array =  generated_signal,
                                                 source_impedance = generator.output_impedance,
                                                 load_impedance = amplifier.input_impedance, 
                                                 signal_baseline = 0.0,
-                                                max_round_trips = 100
+                                                max_round_trips = 5
 
                                                 )
 
@@ -70,7 +81,7 @@ _, voltage_array = cable_to_amplifier.propagation( time_array = time_array,
 """====================== AMPLIFIER INITIALIZATION ======================"""
 
 _, amplified_signal = amplifier.amplify(time_array = time_array,
-                                        loaded_voltage_array = voltage_array,
+                                        loaded_voltage_array = amplifier_input_signal,
                                         signal_baseline = 0.0,
                                         )
 
@@ -78,17 +89,16 @@ _, amplified_signal = amplifier.amplify(time_array = time_array,
 """====================== CABLE 2 INITALIZATION ======================"""
 
 cable_2_length_meters = 5
-velocity_factor = .7
-attenuation_db_per_m = 50
+
 
 cable_to_digitizer = Cable( length_m = cable_2_length_meters,
                             velocity_factor = velocity_factor,
                             attenuation_db_per_m = attenuation_db_per_m,
-                            characteristic_impedance = 50
+                            characteristic_impedance = cable_characteristic_impedance
                             )
 
-_, voltage_array = cable_to_digitizer.propagation( time_array = time_array, 
-                                                open_circuit_voltage_array =  generated_signal,
+_, digitizer_input_signal = cable_to_digitizer.propagation( time_array = time_array, 
+                                                open_circuit_voltage_array =  amplified_signal,
                                                 source_impedance = amplifier.output_impedance,
                                                 load_impedance = digitizer.input_impedance, 
                                                 signal_baseline = 0.0,
@@ -98,21 +108,53 @@ _, voltage_array = cable_to_digitizer.propagation( time_array = time_array,
 """====================== DIGITIZER INITIALIZATION ======================"""
 
 discrete_times, Digitized_array, Reconstructed_array, was_clipped = digitizer.digitize(time_array = time_array,
-                                      loaded_voltage_array = voltage_array,
-                                      sampling_rate_Hz = 2e9, 
-                                      num_bits = 12,
-                                      min_volts = -1.0,
-                                      max_volts = 1.0) 
+                                                                                        loaded_voltage_array = digitizer_input_signal,
+                                                                                        sampling_rate_Hz = 3.2e9, 
+                                                                                        num_bits = 12,
+                                                                                        min_volts = -1.25,
+                                                                                        max_volts = 1.25,
+                                                                                        dc_offset = 0.0) 
 
-plt.plot(time_array, generated_signal, color="blue", label="Original Signal")
-plt.plot(time_array, amplified_signal, color="white", label="Amplified Signal")
-plt.plot(discrete_times, Reconstructed_array, color="red", label="Digitized Signal")
+plt.plot(
+    time_array * 1e9,
+    generated_signal * 1e3,
+    color = "blue",
+    label = "PMT open-circuit output"
+)
 
-plt.xlabel("Time (s)")
-plt.ylabel("Amplitude of Photomultiplier tube Signal (V)")
+plt.plot(
+    time_array * 1e9,
+    amplifier_input_signal * 1e3,
+    color = "cyan",
+    label = "Amplifier input after 1 m cable"
+)
+
+plt.plot(
+    time_array * 1e9,
+    amplified_signal * 1e3,
+    color = "white",
+    label = "Amplifier open-circuit output"
+)
+
+plt.plot(
+    time_array * 1e9,
+    digitizer_input_signal * 1e3,
+    color = "orange",
+    label = "Digitizer analog input"
+)
+
+plt.step(
+    discrete_times * 1e9,
+    Reconstructed_array * 1e3,
+    color = "red",
+    label = "Digitizer samples"
+)
+
+plt.xlabel("Time (ns)")
+plt.ylabel("Voltage (mV)")
+plt.title("PMT Analog Signal Chain")
 
 plt.legend()
-plt.grid(True)
+# plt.grid(True)
 
 plt.show()
-
